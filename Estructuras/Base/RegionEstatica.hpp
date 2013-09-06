cc3205968f9bac5bff296a9efae6c97e9c2a3330
Gabriel Garcia <garciat@live.com>
2013-09-06 00:25:06 -0400

    Adicion de constexpr

--- Estructuras/Base/RegionEstatica.hpp
+++ Estructuras/Base/RegionEstatica.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
+#include "../../defs.h"
 #include <cstddef>
 
 namespace IS220 { namespace Estructuras { namespace Base {
-
+	
 	template <typename TDato, std::size_t UCapacidad>
 	class RegionEstatica {
 	public:
@@ -12,25 +13,25 @@ namespace IS220 { namespace Estructuras { namespace Base {
 		typedef const TDato& const_referencia;
 		typedef TDato* puntero;
 		typedef TDato* const_puntero;
-
-		referencia operator[] (std::size_t pos) {
+		
+		inline referencia operator[] (std::size_t pos) {
 			return * reinterpret_cast<puntero>(&m_region[pos * sizeof(tipo_valor)]);
 		}
-
-		const_referencia operator[] (std::size_t pos) const {
+		
+		inline CONSTEXPR const_referencia operator[] (std::size_t pos) const {
 			return * reinterpret_cast<const_puntero>(&m_region[pos * sizeof(tipo_valor)]);
 		}
-
-		operator puntero() {
+		
+		inline operator puntero() {
 			return & operator[](0u);
 		}
-
-		operator const_puntero() const {
+		
+		inline CONSTEXPR operator const_puntero() const {
 			return & operator[](0u);
 		}
-
+		
 	private:
 		unsigned char m_region[UCapacidad * sizeof(tipo_valor)];
 	};
-
+	
 }}}
--- defs.h
+++ defs.h
@@ -1,5 +1,9 @@
+#pragma once
+
 #if __cplusplus > 199711L
 	#define MOVE(a) std::move(a)
+	#define CONSTEXPR constexpr
 #else
 	#define MOVE(a) (a)
+	#define CONSTEXPR
 #endif
