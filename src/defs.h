03e9dc38ae9b8ceb237100b2ed85938d70ee9cee
Gabriel Garcia <garciat@live.com>
2013-09-22 17:27:15 -0400

    macro para noexcept

--- src/defs.h
+++ src/defs.h
@@ -3,8 +3,10 @@
 #if __cplusplus > 199711L
 	#define MOVE(a) std::move(a)
 	#define CONSTEXPR constexpr
+	#define NOEXCEPT noexcept
 #else
 	#define MOVE(a) (a)
 	#define CONSTEXPR
+	#define NOEXCEPT throw()
 #endif
 
