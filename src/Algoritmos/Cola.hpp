12025d976fe1804a81774b4582ac2644217638de
Gabriel Garcia <garciat@live.com>
2013-09-24 19:51:41 -0400

    No se utilizaran interfaces (por el momento)

--- src/Algoritmos/Cola.hpp
+++ src/Algoritmos/Cola.hpp
@@ -4,47 +4,46 @@
 #include "../Estructuras/Estaticas/ColaEstatica.hpp"
 
 namespace IS220 { namespace Algoritmos {
+
+/*
+
+Se implementa un algoritmo divide-and-conquer estilo quicksort.
+
+*/
+template <class TCola>
+void OrdenarCola(TCola& cola) {
+	if (cola.Vacia()) {
+		return;
+	}
 	
-	/*
+	TDato pivote;
+	cola.Sacar(pivote);
 	
-	Se implementa un algoritmo divide-and-conquer estilo quicksort.
+	// TODO: utilizar colas dinamicas
+	ColaEstatica<TDato> menores, mayores;
 	
-	*/
-	template <class TDato>
-	void OrdenarCola(ICola<TDato>& cola) {
-		if (cola.Vacia()) {
-			return;
-		}
-		
-		TDato pivote;
-		cola.Sacar(pivote);
-		
-		// TODO: utilizar colas dinamicas
-		ColaEstatica<TDato> menores, mayores;
-		
-		TDato elem;
-		
-		while (cola.Sacar(elem)) {
-			if (elem < pivote) {
-				menores.Poner(elem);
-			} else {
-				mayores.Poner(elem);
-			}
-		}
-		
-		OrdenarCola(menores);
-		OrdenarCola(mayores);
-		
-		while (menores.Sacar(elem)) {
-			cola.Poner(elem);
-		}
-		
-		cola.Poner(pivote);
-		
-		while (mayores.Sacar(elem)) {
-			cola.Poner(elem);
+	TDato elem;
+	
+	while (cola.Sacar(elem)) {
+		if (elem < pivote) {
+			menores.Poner(elem);
+		} else {
+			mayores.Poner(elem);
 		}
 	}
 	
-}}
+	OrdenarCola(menores);
+	OrdenarCola(mayores);
+	
+	while (menores.Sacar(elem)) {
+		cola.Poner(elem);
+	}
+	
+	cola.Poner(pivote);
+	
+	while (mayores.Sacar(elem)) {
+		cola.Poner(elem);
+	}
+}
 
+}} // namespace IS220::Algoritmos
