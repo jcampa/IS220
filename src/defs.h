bf1ae4eb0378ac38b2a6ff70e0fffac2add0acae
jcampa <jdelacampa101@gmail.com>
2013-10-23 08:08:45 -0700

    Definicion booleana para C++11

--- src/defs.h
+++ src/defs.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #if __cplusplus > 199711L
+	#define CXX11 1
 	#define MOVE(a) std::move(a)
 	#define CONSTEXPR constexpr
 	#define NOEXCEPT noexcept
 #else
+	#define CXX11 0
 	#define MOVE(a) (a)
 	#define CONSTEXPR
 	#define NOEXCEPT throw()
